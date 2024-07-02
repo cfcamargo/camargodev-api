07e667035efeacf532023fbeb81fab1b996c522b
Cristian Camargo <contato@camargodev.com.br>
2024-07-02 16:19:30 -0400

    feat: ajusta codigo

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,4 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
-const AuthController = () => import('#controllers/auth_controller')
+const AuthController = () => import('#controllers/auth/auth_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 
