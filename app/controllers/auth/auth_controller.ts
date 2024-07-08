27897c722bae9dfa5427864591e9c9b042a9156f
Cristian Camargo <contato@camargodev.com.br>
2024-07-08 15:32:24 -0400

    feat: ajusta sistema auth

--- app/controllers/auth/auth_controller.ts
+++ app/controllers/auth/auth_controller.ts
@@ -6,16 +6,23 @@ export default class AuthController {
   async register({ request }: HttpContext) {
     const data = await request.validateUsing(registerValidator)
     const user = await User.create(data)
-    return User.accessTokens.create(user, ['*'], {
+    const token = await User.accessTokens.create(user, ['*'], {
       expiresIn: '30 days',
     })
+    return {
+      user,
+      token,
+    }
   }
 
   async login({ request }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(email, password)
-
-    return User.accessTokens.create(user)
+    const token = await User.accessTokens.create(user)
+    return {
+      user,
+      token,
+    }
   }
 
   async logout({ auth }: HttpContext) {
